860970c4080b10e2f7f6e45687dc12e719164190
sejong1 <imrere@hanmail.net>
2017-04-19 22:07:15 +0900

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,14 @@
+#"my_module.py" file
+#Other program may use this file
+
+def sumOfDouble(arg1, arg2):
+  print "sumOfDouble function in my_module"
+  val1 = arg1 * 2
+  val2 = arg2 * 2
+  return val1 + val2
+
+def multipleOfDouble(arg1, arg2):
+  print "multipleOfDouble function in my_module"
+  val1=arg1*2
+  val2=arg2*2
+  return val1*val2
